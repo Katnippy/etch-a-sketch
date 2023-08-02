6dcd7a85055f26f23262fa33a917aac02750261d
kat <kat@pop-os.localdomain>
2023-08-02 11:37:27 +0100

    Add cells.

---
+++ script.js
@@ -0,0 +1,21 @@
+function drawColumn(cells) {
+  const container = document.querySelector('#container');
+  const column = document.createElement('div');
+  column.className = 'column';
+  column.appendChild(cells);
+  container.appendChild(column);
+}
+
+function drawCells(amount) {
+  const cells = document.createDocumentFragment();
+  for (let i = 0; i <= amount; i++) {
+    if (i > 0 && i % 16 === 0) {
+      drawColumn(cells)
+    }
+    const cell = document.createElement('div');
+    cell.className = 'cell';
+    cells.appendChild(cell);
+  }
+} 
+
+drawCells(256);
